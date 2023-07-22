b9d1ef76e5ddcfc3175743be67c535d8f298a822
TURAL10 <124426123+TURAL10@users.noreply.github.com>
2023-07-22 04:23:24 +0400

    final proyekt

---
+++ Assets/js/main.js
@@ -0,0 +1,17 @@
+
+function toggleDropdown() {
+    if (document.getElementById("dropdownList").style.display === "block") {
+        document.getElementById("dropdownList").style.display = "none";
+        
+    } else {
+        document.getElementById("dropdownList").style.display = "block";
+    }
+}
+function toggleDrop() {
+    if (document.getElementById("dropdownList").style.display === "block") {
+        document.getElementById("dropdownList").style.display = "none";
+        
+    } else {
+        document.getElementById("dropdownList").style.display = "none";
+    }
+}
